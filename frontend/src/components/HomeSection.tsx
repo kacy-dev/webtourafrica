429e79db0d0f01da6a73452ab7d44d88591e3e3e
kacy-dev <kelechukwuvictor414@gmail.com>
2025-10-06 14:10:39 +0100

    add files for section in components folder

---
+++ frontend/src/components/HomeSection.tsx
@@ -0,0 +1,16 @@
+
+import Navbar from "./Navbar";
+import AboutSection from "./AboutSection";
+import FaqsSection from "./FaqsSection";
+import FooterSection from "./FooterSection";
+
+export default function HomeSection() {
+
+  return (
+    <div className="">
+     <p className="text-red-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum eligendi ut ipsam nobis blanditiis architecto dolore sed possimus numquam officia, perspiciatis ducimus aspernatur. Illo maiores temporibus esse? Cum, fugiat eveniet!</p>
+      <h1 className="text-primary text-2xl font-bold">Welcome to WebTour Africa</h1>
+
+    </div>
+  );
+}
